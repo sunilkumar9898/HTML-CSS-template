037b5fc36373f2e389b026f7d2a295101bcaa728
sunil kumar <webdevlopersunil1997@gmail.com>
2023-12-29 15:03:23 +0530

    update code

---
+++ script.js
@@ -0,0 +1,8 @@
+window.onscroll = function(){
+    var navbar = document.getElementById("navbar");
+    if(window.pageYOffset > 0){
+        navbar.classList.add("sticky");
+    }else{
+        navbar.classList.remove("sticky");
+    }
+}
